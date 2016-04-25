ccf3cc0111398cf81934708432eca066c1fe0559
ao-streetart <aostreetart9@gmail.com>
2016-04-25 02:50:30 -0400

    Initial Commit

---
+++ 0-meter.py
@@ -0,0 +1,363 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  5 23:47:16 2016
+
+-0-Meter Main Class- 
+
+0-Meter is a configurable load testing framework for 0MQ-Based 
+Messaging Applications.
+
+It reads from a configuration XML to determine how to proceed with test cases
+
+We support sending an individual file as well as defining variables and
+updating values with those from a separate CSV.
+
+@author: alex
+"""
+
+import xml.etree.ElementTree as ET
+import sys
+import logging
+import zmq
+import os
+import csv
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+
+import time
+
+#Global Variables
+msg_list = []        
+socket = None
+num_msg = 0
+base_msg = ""
+
+#Global Variables for tracking response times
+resp_time_list = []
+time_list = []
+
+#Config Variables        
+out_0mq_connect = None
+out_0mq_connect_type = None
+
+log_file = None
+log_level = None
+
+msg_location = None
+msg_folder_location = None
+msg_extension = None
+csv_location = None
+csv_var_start = None
+csv_var_end = None
+interval = None
+
+single_message = False
+multi_message = False
+include_csv = False
+span_interval = False
+
+def build_base_msg(msg_path):
+    #Open the base message File 
+    global base_msg
+    try:
+        with open(msg_path, 'r') as f:
+            global base_msg
+            base_msg = f.read()
+            logging.info("Base Message file opened")
+    except Exception as e:
+        logging.error('Exception during read of base message')
+        logging.error(e)
+
+def build_msg_list_from_csv(msg_path, config_csv):
+    
+    global msg_list
+    
+    #List of replacement variables & values
+    #We do not insert or remove, only append and clear which means we can assume
+    #that they have the same ordering
+    repl_variables = []
+    repl_values = []
+    
+    #Counter variable
+    file_id_counter=0
+    row_counter=0
+    char_counter=0
+    
+    #Open the base message File 
+    try:
+        with open(msg_path, 'r') as f:
+            msg_string = f.read()
+            logging.info("Base Message file opened")
+    except Exception as e:
+        logging.error('Exception during read of base message')
+        logging.error(e)
+        msg_string = ""
+    
+    #Open the CSV File and start building Message Files
+    with open(config_csv, 'rb') as csvfile:
+        logging.info('CSV File Opened')
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in reader:
+            #We can access elements in the row with row[i], where i = column number
+            #First Row is assumed to contain headers
+            if row_counter==0:
+                #This is a header row
+                for element in row:
+                    repl_variables.append(element)
+                    logging.info('Replacement Variable %s added' % (element))
+            else:
+                #Process the row and build a new Message
+            
+                #Populate the repl_values list
+                for element in row:
+                    repl_values.append(element)
+                
+                #Build the string message to be written to the output file
+                new_str = msg_string
+
+                str_length = len(new_str)
+                while char_counter < str_length:
+                    if new_str[char_counter] == csv_var_start:
+                        #We have found a replacement variable
+                        #We need to replace this block with the
+                        #corresponding repl_value
+                        logging.debug('Replacement variable found in base message')
+                        var_start=char_counter
+                        var_end=0
+                        var_name=""
+                        found_back=False
+                        var_id=-1
+                        while found_back==False and len(new_str) > char_counter:
+                            logging.debug('Find full variable loop entered')
+                            char_counter+=1
+                            if new_str[char_counter] == csv_var_end:
+                                found_back=True
+                                var_end=char_counter
+                                logging.debug('Var Start Position: %s' % (var_start))
+                                logging.debug('Var End Position: %s' % (var_end))
+                            else:
+                                var_name = var_name + new_str[char_counter]
+                                logging.debug('Character added: %s' % (new_str[char_counter]))
+                        i=0
+                        for i in range(0, len(repl_variables)):
+                            if repl_variables[i] == var_name:
+                                var_id=i
+                                
+                        #We only want to perform a replacement if we found a matching variable name
+                        if var_id > -1:
+                            new_str = new_str[:var_start] + repl_values[var_id] + new_str[var_end+1:]
+                            str_length = len(new_str)
+                            char_counter=var_start
+                            
+                        logging.debug('Character Counter')
+                    else:
+                        char_counter+=1
+                    
+                msg_list.append(new_str)
+                
+                #Clear the repl_values array for the next iteration
+                del repl_values[:]
+                
+                #Zero out the character counter for the next iteration
+                char_counter=0
+                
+                #Iterate the file counter
+                file_id_counter+=1
+                
+            row_counter+=1
+
+def select_files_in_folder(dir, ext):
+    for file in os.listdir(dir):
+        if file.endswith('.%s' % ext):
+            yield os.path.join(dir, file)
+            
+def touch(file_path):
+    open(file_path, 'a').close()
+
+def post_message():
+    global resp_time_list
+    global time_list
+    global msg_list
+    global socket
+    if len(msg_list) > 0:
+        try:
+            #Send the message
+            msg = msg_list.pop(0)
+            time_list.append(time.time())
+            socket.send_string(msg)
+            
+            #Recieve the response 
+            resp = socket.recv()
+            resp_time_list.append(time.time())
+            logging.info("Response Recieved:")
+            logging.info(resp)
+        except Exception as e:
+            logging.error("Exception encountered during message post")
+            logging.error(e)
+def execute_main():
+    global base_msg
+    global msg_list
+    global socket
+    global num_msg
+    global out_0mq_connect
+    global out_0mq_connect_type
+    global log_file
+    global log_level
+    global msg_location
+    global msg_folder_location
+    global csv_location
+    global interval
+    global single_message
+    global multi_message
+    global include_csv
+    global span_interval
+    global msg_extension
+    global csv_var_start
+    global csv_var_end
+    if len(sys.argv) == 1:
+        print("Input Parameters:")
+        print("Configuration File: The file name of the Configuration XML")
+        print("Example: python 0-meter.py config.xml")
+    elif len(sys.argv) != 2:
+        
+        print("Wrong number of Input Parameters")
+        
+    else:
+    
+        print("Input Parameters:")
+        print("Configuration File: %s" % (sys.argv[1]))
+        
+    #-----------------------------------------------------------------------------#
+    #-----------------------------------------------------------------------------#
+        
+        #Parse the config XML and pull the values
+        
+        tree = ET.parse(sys.argv[1])
+        root = tree.getroot()
+        for element in root:
+            if element.tag == 'Behavior':
+                for param in element:
+                    if param.tag == 'Single_Message':
+                        if param.text == 'True':
+                            single_message = True
+                    if param.tag == 'Multi_Message':
+                        if param.text == 'True':
+                            multi_message = True
+                    if param.tag == 'Include_CSV':
+                        if param.text == 'True':
+                            include_csv = True
+                    if param.tag == 'Span_Over_Interval':
+                        if param.text == 'True':
+                            span_interval = True
+            if element.tag == 'Message':
+                for param in element:
+                    if param.tag == 'Message_Location':
+                        msg_location = param.text
+                    if param.tag == 'Message_Folder_Location':
+                        msg_folder_location = param.text
+                    if param.tag == 'Message_Extension':
+                        msg_extension = param.text
+                    if param.tag == 'Interval':
+                        interval = float(param.text)
+                    if param.tag == 'CSV_Location':
+                        csv_location = param.text
+                    if param.tag == 'Variable_Start_Character':
+                        csv_var_start = param.text
+                    if param.tag == 'Variable_End_Character':
+                        csv_var_end = param.text
+            if element.tag == 'ZeroMQ':
+                for param in element:
+                    if param.tag == 'Outbound_Connection':
+                        out_0mq_connect = param.text
+                    if param.tag == 'Outbound_Connection_Type':
+                        out_0mq_connect_type = param.text
+            if element.tag == 'Logging':
+                for param in element:
+                    if param.tag == 'Log_File':
+                        log_file = param.text
+                    elif param.tag == 'Log_Level':
+                        log_level = param.text
+        
+        #Set up the file logging config
+        if log_level == 'Debug':
+            logging.basicConfig(filename=log_file, level=logging.DEBUG)
+        elif log_level == 'Info':
+            logging.basicConfig(filename=log_file, level=logging.INFO)
+        elif log_level == 'Warning':
+            logging.basicConfig(filename=log_file, level=logging.WARNING)
+        elif log_level == 'Error':
+            logging.basicConfig(filename=log_file, level=logging.ERROR)
+        else:
+            print("Log level not set to one of the given options, defaulting to debug level")
+            logging.basicConfig(filename=log_file, level=logging.DEBUG)
+            
+        try:
+            #Attempt to connect to the outbound ZMQ Socket
+            logging.debug("Attempting to connect to outbound 0MQ Socket with connection:")
+            logging.debug(out_0mq_connect)
+            context = zmq.Context()
+            if out_0mq_connect_type == "REQ":
+                socket = context.socket(zmq.REQ)
+                socket.connect(out_0mq_connect)
+            elif out_0mq_connect_type == "PUB":
+                socket = context.socket(zmq.PUB)
+                socket.connect(out_0mq_connect)
+            else:
+                logging.error("Unknown Connection Type encountered")
+                return 0
+            
+        #If an exception is thrown while executing the tests,
+        #write that out to the log file
+        except Exception as e:
+            logging.error('Exception')
+            logging.error(e)
+            print("Exception encountered connecting to 0MQ Socket, please see logs for details")
+            return 0
+
+        #Now, we need to determine how many messages we're sending and build them
+        if single_message:
+            logging.debug("Building Single Message")
+            num_msg=1
+            build_base_msg(os.path.abspath(msg_location))
+            msg_list.append(base_msg)
+        elif multi_message == True and include_csv == True:
+            logging.debug("Building Messages from CSV")
+            #Pull the correct file paths
+            msg_path = os.path.abspath(msg_location)
+            config_csv = os.path.abspath(csv_location)
+            
+            #Read the CSV, Build the message list, and take it's length for num_msg
+            build_msg_list_from_csv(msg_path, config_csv)
+            num_msg=len(msg_list)
+        
+        elif multi_message:
+            logging.debug("Building Messages from Folder")
+            msg_folder = select_files_in_folder(os.path.abspath(msg_folder_location), msg_extension)
+            
+            #Build the message list
+            for msg in msg_folder:
+                build_base_msg(os.path.abspath(msg))
+                msg_list.append(base_msg)
+                
+            num_msg = len(msg_list)
+
+        #Now, we can execute the test plan
+        if span_interval == False:
+            logging.debug("Sending Messages all at once")
+            while len(msg_list) > 0:
+                post_message()
+        else:
+            logging.debug("Set up the Background Scheduler")
+            scheduler = BackgroundScheduler()
+            time_interv = num_msg / interval
+            logging.debug("Interval: %s" % (time_interv))
+            interv = IntervalTrigger(seconds=time_interv)
+            scheduler.add_job(post_message, interv)
+            scheduler.start()
+            time.sleep(interval + time_interv)
+    return 0;
+
+
+if __name__ == "__main__":
+    execute_main()
---
+++ README.md
@@ -0,0 +1,131 @@
+# 0-Meter
+
+0-Meter is heavily influenced by JMeter, and is designed to be a load testing application for 0MQ-Based Applications.
+
+The python program can read from flat files to build messages, as well as substitute variables found in a CSV file.  It can transmit one or more messages to a server all at once, or at scheduled intervals.
+
+The execution of the program is driven by a configuration file, which is read at startup.  Details are given below on the file.
+
+## Features
+* Send a single 0MQ Request Message with content from a flat file
+* Send a set of 0MQ Request Messages with content from a set of flat files contained in a folder
+* Send a set of 0MQ Request Messages with base content from a flat file, and variables substituted from a CSV file
+* Send a set of 0MQ Request Messages, with content from files in a folder of generated from a CSV, scheduled periodically over a specified interval of time.
+
+## Dependencies
+
+Before downloading 0-Meter, you will need a few things installed:
+
+* Python 2.7 - Can be downloaded [here] (https://www.python.org/)
+* pyzmq
+* apscheduler
+
+Once you download Python, you can run the below commands to install the other dependencies:
+
+`pip install apscheduler`
+
+`pip install pyzmq`
+
+## Use
+
+0-Meter is designed to be easy to use.  It is a command line python script, and all of the software is contained within the .py file.  This only has a few dependencies to resolve, which can be found above in the 'Dependencies' segment.
+
+Once the dependencies are installed, using the script is as easy as running the below command:
+
+`python 0-meter.py config.xml`
+
+The configuration XML will vary based on which XML you use, this command will work for the configuration XML provided
+
+## Configuration
+
+### Overview
+
+Configuration of 0-Meter is done via editing of the configuration XML.  This file contains a number of fields critical to 0-Meter's execution, all of which are described below.
+
+This methodology of configuration was selected to allow for 0-Meter to be run in a headless environment.  If a User Interface is desired, development of one would be easy so long as it simply wrote configuration XML files and then the python script can be executed independently.
+
+Example Configuration Files can be found in the repository as well.
+
+### Segments
+
+There are four segments to the Configuration XML.
+
+* Behavior - This fundamentally drives the behavior of the program
+* Message - This drives how we generate messages to send
+* ZeroMQ - Connectivity Information for ZeroMQ
+* Logging - Logging Information
+
+### Suggested Configurations
+
+Suggested Configurations include True/False flags for Behavior Segment.  It also includes descriptions for required elements in the Message Segment.
+
+All configurations require valid entries for all elements in both ZeroMQ and Logging Segments.
+
+#### Send One Message from a Flat File
+
+- [x] Single_Message
+- [] Multi_Message
+- [] Include_CSV
+- [] Span_Over_Interval
+
+Here we send a single message, read from a flat file, on the specified port.  The below values are read from the Message Segment:
+
+* Message_Location - The filename & location of the message being sent
+
+#### Send A Collection of Messages from Flat Files in a Folder
+
+- [] Single_Message
+- [x] Multi_Message
+- [] Include_CSV
+- [] Span_Over_Interval
+
+Here we send a collection of messages, all at once, read from a collection of flat files in a folder, on the specified port.  The below values are read from the Message Segment:
+
+* Message_Folder_Location - The location of the folder containing the messages being sent
+* Message_Extension - The extension to look for in the specified file for messages
+
+#### Send A Collection of Messages from Flat Files in a Folder, Scheduled over a specified interval of time
+
+- [] Single_Message
+- [x] Multi_Message
+- [] Include_CSV
+- [x] Span_Over_Interval
+
+Here we send a collection of messages, all at once, read from a collection of flat files in a folder, on the specified port. These messages are sent periodically, with the formula being one message per (number_of_messages / interval) seconds. The below values are read from the Message Segment:
+
+* Message_Folder_Location - The location of the folder containing the messages being sent
+* Message_Extension - The extension to look for in the specified file for messages
+* Interval - The overall interval for messages to be sent
+
+#### Send A Collection of Messages from a Flat File & CSV
+
+- [] Single_Message
+- [x] Multi_Message
+- [x] Include_CSV
+- [] Span_Over_Interval
+
+Here we send a collection of messages, all at once, on the specified port.  The messages are created by reading a base message, parsing it for defined variables, and substiting them with values from the CSV.  Variables are simply names that match the headers of the CSV File, starting with the Variable_Start_Character and ending with the Variable_End_Character.  The below values are read from the Message Segment:
+
+* Message_Location - The location of the base message
+* CSV_Location - The location & Filename of the CSV to read for variable data
+* Variable_Start_Character - The start character of variables in the CSV
+* Variable_End_Character - The end character of variables in the CSV
+
+#### Send A Collection of Messages from a Flat File & CSV, Scheduled over a specified interval of time
+
+- [] Single_Message
+- [x] Multi_Message
+- [x] Include_CSV
+- [x] Span_Over_Interval
+
+Here we send a collection of messages, all at once, on the specified port.  The messages are created by reading a base message, parsing it for defined variables, and substiting them with values from the CSV.  Variables are simply names that match the headers of the CSV File, starting with the Variable_Start_Character and ending with the Variable_End_Character.  These messages are sent periodically, with the formula being one message per (number_of_messages / interval) seconds.  The below values are read from the Message Segment:
+
+* Message_Location - The location of the base message
+* CSV_Location - The location & Filename of the CSV to read for variable data
+* Variable_Start_Character - The start character of variables in the CSV
+* Variable_End_Character - The end character of variables in the CSV
+* Interval - The overall interval for messages to be sent
+
+## To-Do List
+* Batch message generation process to allow for larger-scale load testing.
+* Add Google Protocol Buffer support & Serialization support to allow for serialized objects to be sent via 0MQ
